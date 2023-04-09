9d385df4d620be353598e92d8ac59864a78e3603
anovic123 <105565287+anovic123@users.noreply.github.com>
2023-04-09 12:44:18 +0300

    clear data imgSrc

--- src/common/mocks/data.ts
+++ src/common/mocks/data.ts
@@ -1,21 +1,21 @@
 export const data = [
   {
     id: 1,
-    imgSrc: 'https://www.pinkvilla.com/english/images/2023/01/821644923_simp_1280*720.jpg',
+    imgSrc: '',
     date: '10.11.2020 г.',
     title: 'Ким и Валерия Брейтбурги написали книгу про искусство',
     description: 'Работа над книгой велась более пяти лет, и действенные методики, описанные в ней, созданные в результате анализа и синтеза идей…'
   },
   {
     id: 2,
-    imgSrc: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSdeJPbPdj2OjzK6OQP3zsfufTEPhiQ03z9QA&usqp=CAU',
+    imgSrc: '',
     date: '10.11.2020 г.',
     title: 'Юрий Колокольников пытается спасти Землю в клипе «Космические силы»',
     description: 'Премьера клипа «Космические силы» группы «Мумий Тролль» состоялась 6 ноября 2020 года.'
   },
   {
     id: 3,
-    imgSrc: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRR7KTHy18qk25uLKS980GdzrpRVyjiUK2gRA&usqp=CAU',
+    imgSrc: '',
     date: '10.11.2020 г.',
     title: 'BTS получили четыре награды MTV EMA',
     description: 'BTS получили четыре награды MTV EMA 27-я ежегодная церемония награждения MTV Europe Music Awards состоялась 8 ноября 2020 г.'
@@ -29,28 +29,28 @@ export const data = [
   },
   {
     id: 5,
-    imgSrc: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSgUiuu7o1InOoCvWXeMQtHEHhOOyxdp0sAnw&usqp=CAU',
+    imgSrc: '',
     date: '10.11.2020 г.',
     title: 'Ким и Валерия Брейтбурги написали книгу про искусство',
     description: 'Работа над книгой велась более пяти лет, и действенные методики, описанные в ней, созданные в результате анализа и синтеза идей…'
   },
   {
     id: 6,
-    imgSrc: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSMkZwOjpuS7Y7JPDxwtBD11cpSJlt22f6-n3vEYi21pB9DBnsR_rL8OR4j9sh6x4YK04U&usqp=CAU',
+    imgSrc: '',
     date: '10.11.2020 г.',
-    title: 'Юрий Колокольников пытается спасти Землю в клипе «Космические силы»',
+    title: '',
     description: 'Премьера клипа «Космические силы» группы «Мумий Тролль» состоялась 6 ноября 2020 года.'
   },
   {
     id: 7,
-    imgSrc: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSdXEUx2uFzVymA8bCZ_wpLntgSlzXZZ-O1sw&usqp=CAU',
+    imgSrc: '',
     date: '10.11.2020 г.',
     title: 'BTS получили четыре награды MTV EMA',
     description: 'BTS получили четыре награды MTV EMA 27-я ежегодная церемония награждения MTV Europe Music Awards состоялась 8 ноября 2020 г.'
   },
   {
     id: 8,
-    imgSrc: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS-h6bUuFmAWsZA9cruI-ZLj781kjIt45sEVQ&usqp=CAU',
+    imgSrc: '',
     date: '10.11.2020 г.',
     title: 'Открытие сезона «Ла Скала» отменено',
     description: 'Миланский театр La Scala отменил открытие сезона. Первый спектакль был назначен на 7 декабря — «Лючия ди Ламмермур».'
